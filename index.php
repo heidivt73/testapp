8026b6d67de426e2b53f9c87f4500590b886074d
Heidi <heidivt@gmail.com>
2013-11-23 23:26:11 -0800

    fds

--- index.php
+++ index.php
@@ -93,12 +93,12 @@ function createPreview() {
     <![endif]-->
   </head>
   <body>
-	<iframe src="<?php echo createPreview(); ?>" seamless="" style="border:none; width:750px; height:440px;"></iframe>
+<!--	<iframe src="<?php echo createPreview(); ?>" seamless="" style="border:none; width:750px; height:440px;"></iframe> -->
 	<form>
         <input id="urlField" type="text"/>
         <input type="button" value="Create Preview" onClick="setResponseLabel('heidi')"/>
         <span id="responseLabel"><?php echo createPreview(); ?></span>
-        
+        <a href="#" onClick="window.open(document.getElementById('responseLabel').innerHTML)" />
     </form>
     <form>
     	<input type="button" value="test http" onClick="testHTTPRequest()" />
